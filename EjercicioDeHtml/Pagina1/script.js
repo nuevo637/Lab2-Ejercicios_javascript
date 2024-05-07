845004e71c037e737a3bb117ff4f1a96312c1c65
nuevo_63 <ajrahuacso@gmail.com>
2024-05-07 18:45:53 -0500

    Paginas html hechas, funciona el codigo javascript y se completa el primer ejercicio relacionado a las paginas

--- EjercicioDeHtml/Pagina1/script.js
+++ EjercicioDeHtml/Pagina1/script.js
@@ -4,16 +4,21 @@ const btnDisminuir = document.getElementById('disminuir');
 
 btnAumentar.addEventListener('click', () =>{
     let tamaño = parseFloat(window.getComputedStyle(texto).getPropertyValue("font-size"));
-    texto.style.color = "RGB(0,0,0)";
+    texto.style.color = aleatorio();
     texto.style.fontSize = (tamaño + 10) + "px";
 }); 
 
 btnDisminuir.addEventListener('click', () =>{
     let tamaño = parseFloat(window.getComputedStyle(texto).getPropertyValue("font-size"));
-    texto.style.color = "yellow";
+    texto.style.color = aleatorio();
     texto.style.fontSize = (tamaño - 10) + "px";
 }); 
 
 function aleatorio(){
-    return Math.random() * (255 - 0) + min;
+    let numero1 = Math.random() * (255);
+    let numero2 = Math.random() * (255);
+    let numero3 = Math.random() * (255);
+    
+    let rgb = "RGB(" + numero1 + "," + numero2 + ","+ numero3 +")";
+    return rgb;
 }
