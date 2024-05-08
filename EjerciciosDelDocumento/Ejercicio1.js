2db32cee779a8832fa94c67c0bb30c3e415742d0
nuevo_63 <ajrahuacso@gmail.com>
2024-05-07 19:00:27 -0500

    Ejercicio 1 de Javascript, documento de laboratorio

---
+++ EjerciciosDelDocumento/Ejercicio1.js
@@ -0,0 +1,8 @@
+function diaActual(){
+    let date = new Date();
+    let day = date.getDay();
+    const arr = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Sabado"];
+    return arr[day];
+}
+
+console.log(diaActual());
