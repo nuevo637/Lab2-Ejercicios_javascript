4ec54ebcc285a7cf16f43c7028aaf0967eb16e1b
nuevo_63 <ajrahuacso@gmail.com>
2024-05-09 01:14:30 -0500

    Ejercicio 3 de laboratorio javascript, el javascript ya funciona

--- EjerciciosDelDocumento/Ejercicio3/ejercicio3.js
+++ EjerciciosDelDocumento/Ejercicio3/ejercicio3.js
@@ -3,13 +3,19 @@ function obtenerDias(año, mes) {
     return cantDias.getDate();
 }
 
+function diasFaltantes(año, mes, dia){
+    let diaArequipa = new Date(año, 7, 15)
+    let diasTotal = obtenerDias(año, mes) - dia;
+    while (mes < diaArequipa.getMonth()) {
+        diasTotal += obtenerDias(año, mes);
+        mes += 1;
+    }
+    diasTotal -= diaArequipa.getDate() + 1;
+    return diasTotal;
+}
 
-
-
-
-let fechaActual = new Date();
-let mesActual = fechaActual.getMonth();
-let diaActual = fechaActual.getDate();
-
-
-console.log(obtenerDias(hoy.getFullYear(), 5));
+let hoy = new Date();
+let año = hoy.getFullYear();
+let dia = hoy.getDate();
+let mes = hoy.getMonth();
+console.log(diasFaltantes(año, mes, dia))
