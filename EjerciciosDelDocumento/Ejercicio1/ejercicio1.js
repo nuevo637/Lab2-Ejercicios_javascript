1e9c257c86b77dbd01b2479c91d72acd913a9b11
nuevo_63 <ajrahuacso@gmail.com>
2024-05-11 18:46:35 -0500

    Se agregan elementos al informe de latex y se genera el PDF del informe

--- EjerciciosDelDocumento/Ejercicio1/ejercicio1.js
+++ EjerciciosDelDocumento/Ejercicio1/ejercicio1.js
@@ -1,6 +1,6 @@
 function diaActual(){
     let date = new Date();
-    let day = date.getDay();
+    let day = date.getDay() - 1;
     const arr = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Sabado"];
     return arr[day];
 }
